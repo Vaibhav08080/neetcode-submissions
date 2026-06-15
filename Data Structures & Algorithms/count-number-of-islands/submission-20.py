b42d8866ef17649bb4f9fb08b738018a3d07f41d
vaibhav <vibhuuu05@gmail.com>
2026-06-15 16:26:13 +0530

    Add: count-number-of-islands - submission-20

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-20.py
@@ -0,0 +1,31 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        """
+        ["0","1","1","1","0"],
+        ["0","1","0","1","0"],
+        ["1","1","0","0","0"],
+        ["0","0","0","0","0"]
+
+        now for each move i can move up down left right
+        directions=[[-1,0] , [1 ,0] , [0 ,1] , [0,-1]]
+
+
+        """
+        directions=[[1, 0] , [0, 1] , [0, -1] , [-1,0]]
+        rows= len(grid)
+        cols=len(grid[0])
+        count=0
+        def dfs(r , c):
+            if (r<0 or c<0 or r>=rows or c>=cols or grid[r][c]=="0"):
+                return
+            grid[r][c]="0"
+            for dr , dc in directions:
+                dfs(r+dr ,c+dc)
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]=="1":
+                    dfs(r, c)
+                    count+=1
+        return count
+                
+            
