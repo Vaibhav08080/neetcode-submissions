65badea43ac3dd24e6cfb027b1ea3e64627ac3ed
vaibhav <vibhuuu05@gmail.com>
2026-06-16 15:12:39 +0530

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        newset=set(nums)
+        if len(newset)==len(nums):
+            return False
+        else:
+            return True
+        
